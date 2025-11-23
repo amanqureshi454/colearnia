f0a4a48b800dcc1352a505d606a701911a8ed816
amanqureshi454 <qureshiamanqureshi@gmail.com>
2025-11-23 12:18:57 +0530

    added

--- src/app/api/checkout_sessions/route.ts
+++ src/app/api/checkout_sessions/route.ts
@@ -1,5 +1,6 @@
 export const dynamic = "force-dynamic";
 export const runtime = "nodejs";
+export const preferredRegion = "auto";
 
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
